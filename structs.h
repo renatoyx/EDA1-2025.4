f29f6b77e2f683cc2bafc4807791d857b2b62679
Jonathan-Carpaneda <jonathancarpanedaj@gmail.com>
2026-01-30 09:25:53 -0300

    structs e arquivos base

---
+++ structs.h
@@ -0,0 +1,40 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+typedef struct Data{
+    int dia;
+    int mes;
+    int ano;
+}Data;
+
+
+typedef struct Cliente{
+    char cpf[12];
+    char nome[100];
+    char email[100];
+    char telefone[100];
+    Data nascimento;
+    struct Cliente * prox;
+}Cliente;
+
+typedef struct Produto{
+    char codigo_unico[100];
+    char nomeP[100];
+    float preco;
+    int quantidade;
+    struct Produto * prox;
+}Produto;
+
+typedef struct ItemNoCarrinho{
+    int coigoUnico;
+    char nomePC[100];
+    float precoPC;
+    int quantidade;
+    struct ItemNoCarrinho * prox;
+}ItemNoCarrinho;
+
+
+
+
+
+#endif
