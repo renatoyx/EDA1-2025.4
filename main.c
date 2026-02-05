33b1ba415e0ecd855a1ffb978ca6ad3325923e51
Renato Araújo Gameiro <renatogameiro1@gmail.com>
2026-02-04 22:02:15 -0300

    refatorando a main

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-#include "clientes.h"
-#include "produtos.h"
+#include <stdlib.h>
+#include "clientes.h" 
+#include "produtos.h" 
+#include "utils.h"  
 
 int main(){
 
@@ -8,25 +10,28 @@ int main(){
     Produto *produtos = NULL;
 
     int opcao;
+    char cpfAux[15]; 
 
     do{
-
-        printf("\n========== SISTEMA ==========\n");
+        printf("\n------- SISTEMA ----------n");
         printf("1 - Cadastrar cliente\n");
         printf("2 - Listar clientes\n");
-        printf("3 - Cadastrar produto\n");
-        printf("4 - listar produtos\n");
-        printf("5 - Editar um produto\n");
-        printf("6 - Buscar produto por codigo\n");
-        printf("7 - Entrar no modo compra\n");
+        printf("3 - Buscar cliente por CPF\n");
+        printf("4 - Editar cliente\n");
+        printf("5 - Remover cliente\n");
+        printf("6 - Cadastrar produto\n");
+        printf("7 - Listar produtos\n");
+        printf("8 - Buscar produto por codigo\n");
+        printf("9 - Editar produto\n");
+        printf("10 - Remover produto\n");
+        printf("11 - Entrar no modo compra\n");
         printf("0 - Sair\n");
 
         printf("Escolha: ");
         scanf("%d", &opcao);
-        limparBuffer();
 
         switch(opcao){
-
+            //  GERENCIAR CLIENTES
             case 1:
                 clientes = cadastrarCliente(clientes);
                 break;
@@ -36,22 +41,49 @@ int main(){
                 break;
 
             case 3:
-                produtos = cadastrarProduto(produtos);
+                printf("Digite o CPF para buscar: ");
+                scanf(" %14s", cpfAux);
+                
+                Cliente *buscado = buscarCliente(clientes, cpfAux);
+                if(buscado){
+                    imprimirCliente(buscado);
+                } else {
+                    printf("Cliente nao encontrado.\n");
+                }
                 break;
 
             case 4:
-                listarProdutos(produtos);
+                printf("Digite o CPF do cliente a ser editado: ");
+                scanf(" %14s", cpfAux);
+                clientes = editarCliente(clientes, cpfAux);
                 break;
 
             case 5:
-                editarProduto(produtos);
+                clientes = removerCliente(clientes, cpfAux);
                 break;
 
             case 6:
+                produtos = cadastrarProduto(produtos);
+                break;
+
+            case 7:
+                listarProdutos(produtos);
+                break;
+
+            case 8:
                 exibirBuscaPorCodigo(produtos);
                 break;
+
+            case 9:
+                editarProduto(produtos);
+                break;
+
+            case 10:
+                produtos = removerProduto(produtos);
+                break;
             
-            case 7:
+            //  MODO COMPRA 
+            case 11:
                 chamarModoCompra(clientes, produtos);
                 break;
 
